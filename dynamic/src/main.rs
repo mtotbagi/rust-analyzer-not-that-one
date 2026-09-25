26c2caf9f99890b34adb033cda171c6dfa28a97d
mtotbagi <mtotbagi@gmail.com>
2026-09-25 10:57:43 +0200

    addded testing small inputs and constants

--- dynamic/src/main.rs
+++ dynamic/src/main.rs
@@ -1,6 +1,7 @@
 use std::{env, fmt::Display};
 
 use interpreter::*;
+use itertools::Itertools;
 use serde_json::Value;
 
 fn main() {
@@ -12,25 +13,57 @@ fn main() {
     let (classname, methodname) = get_class_and_method(&args[1]);
     let json: Value = read_json(&classname);
     let class = Class::from_json(&json);
-    let method = class.get_method(&methodname)
+
+    let method = class
+        .get_method(&methodname)
         .expect(&format!(
             "Method {} should be implemented on {}",
             methodname, classname
         ))
         .clone();
-    if !method.id.params.is_empty() {
-        println!("ok;skip");
-        println!("divide by zero;skip");
-        println!("assertion error;skip");
-        println!("out of bounds;skip");
-        println!("null pointer;skip");
-        println!("*;skip");
-        return;
+    if method
+        .id
+        .params
+        .iter()
+        .any(|param| matches!(param, SimpleType::SimpleRef(_)))
+    {
+        if !method.id.params.is_empty() {
+            println!("ok;skip");
+            println!("divide by zero;skip");
+            println!("assertion error;skip");
+            println!("out of bounds;skip");
+            println!("null pointer;skip");
+            println!("*;skip");
+            return;
+        }
     }
+    let consts: Vec<_> = class
+        .methods
+        .iter()
+        .map(|m| {
+            m.instructions.iter().filter_map(|inst| match inst {
+                Instruction::Push { value } => Some(value.clone()),
+                _ => None,
+            })
+        })
+        .flatten()
+        .collect();
+    let possible_inputs: Vec<Vec<_>> = method
+        .id
+        .params
+        .iter()
+        .map(|param| {
+            let mut inputs = input_for_param(param);
+            let ty = inputs[0].get_type();
+            inputs.extend(consts.iter().filter(|c| c.get_type() == ty));
+            inputs
+        })
+        .collect();
 
-    let input = empty_input();
-    let interpreter = Interpreter::new(class);
-    let (_, res) = interpreter.interpret(&method, input, 100);
+    let input_iter = possible_inputs
+        .into_iter()
+        .map(|vec| vec.into_iter())
+        .multi_cartesian_product();
 
     let mut pred = Prediction {
         ok: "no".to_string(),
@@ -41,18 +74,37 @@ fn main() {
         no_halt: "no".to_string(),
     };
 
-    match res {
-        ExeResult::Ok => pred.ok = "yes".to_string(),
-        ExeResult::AssertErr => pred.assert_err = "yes".to_string(),
-        ExeResult::OutOfBounds => pred.out_of_bounds = "yes".to_string(),
-        ExeResult::NullPointer => pred.null_pointer = "yes".to_string(),
-        ExeResult::Div0 => pred.div0 = "yes".to_string(),
-        ExeResult::NoHalt => pred.no_halt = "yes".to_string(),
-        ExeResult::DidNotFinish => pred.no_halt = "probably".to_string(),
+    let interpreter = Interpreter::new(class);
+    for input in input_iter {
+        let (_, res) = interpreter.interpret(&method, (input, Heap { heap: vec![] }), 1000);
+        match res {
+            ExeResult::Ok => pred.ok = "yes".to_string(),
+            ExeResult::AssertErr => pred.assert_err = "yes".to_string(),
+            ExeResult::OutOfBounds => pred.out_of_bounds = "yes".to_string(),
+            ExeResult::NullPointer => pred.null_pointer = "yes".to_string(),
+            ExeResult::Div0 => pred.div0 = "yes".to_string(),
+            ExeResult::NoHalt => pred.no_halt = "yes".to_string(),
+            ExeResult::DidNotFinish => {
+                if pred.no_halt != "yes" {
+                    pred.no_halt = "probably".to_string()
+                }
+            }
+        }
     }
     println!("{}", pred)
 }
 
+fn input_for_param(param: &SimpleType) -> Vec<StackValue> {
+    match param {
+        SimpleType::Int => (-100..100).map(|i| StackValue::Int(i)).collect(),
+        SimpleType::Float => todo!(),
+        SimpleType::Byte => (0..256).map(|i| StackValue::Int(i)).collect(),
+        SimpleType::Char => (0..256).map(|i| StackValue::Int(i)).collect(),
+        SimpleType::Short => (-100..100).map(|i| StackValue::Int(i)).collect(),
+        SimpleType::Boolean => (0..=1).map(|i| StackValue::Int(i)).collect(),
+        SimpleType::SimpleRef(_) => todo!(),
+    }
+}
 
 struct Prediction {
     ok: String,
